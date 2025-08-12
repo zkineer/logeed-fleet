b9c3ea06fd68a7b7191236d5252d2226ea51601d
Angel <angel@logeed.com>
2025-08-11 19:08:22 -0600

    Menu fix 1.0

--- src/Core/Header.php
+++ src/Core/Header.php
@@ -1,11 +1,15 @@
 <header class="nav-header closed" id="navHeader">
     <div class="menu-icon" id="toggleMenu">
         <!-- SVG Camión -->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 100 100"
+             width="35" height="35"
+             style="vertical-align: middle;">
             <line x1="10" y1="30" x2="90" y2="10" stroke="red" stroke-width="6" stroke-linecap="round"/>
             <line x1="10" y1="50" x2="90" y2="30" stroke="red" stroke-width="6" stroke-linecap="round"/>
             <line x1="10" y1="70" x2="90" y2="50" stroke="red" stroke-width="6" stroke-linecap="round"/>
         </svg>
+
 
     </div>
 
